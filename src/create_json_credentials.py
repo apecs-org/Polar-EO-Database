f629219eba9daa0465f4cb4d0311edee4801e601
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-03-21 23:40:20 +0100

    fix yaml

--- src/create_json_credentials.py
+++ src/create_json_credentials.py
@@ -16,11 +16,11 @@ argument_list = str(sys.argv)
 # create dict with input credentials and some more variables
 json_credentials = {
     "type": "service_account",
-    "project_id": argument_list[0],
-    "private_key_id": argument_list[1],
-    "private_key": argument_list[2],
-    "client_email": argument_list[3],
-    "client_id": argument_list[4],
+    "project_id": os.environ["PROJECT_ID"],
+    "private_key_id": os.environ["PRIVATE_KEY_ID"],
+    "private_key": os.environ["PRIVATE_KEY"],
+    "client_email": os.environ["CLIENT_EMAIL"],
+    "client_id": os.environ["CLIENT_ID"],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
