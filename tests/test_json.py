e82a65e9dd4b48895c9c2bdeee0c9cf1ce3b5ae7
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2022-03-22 15:47:43 +0100

    Working on JSON field testing

--- tests/test_json.py
+++ tests/test_json.py
@@ -8,9 +8,51 @@ import json
 import glob
 
 
-def test_file_opening():
-    sat_files = sorted(glob.glob("./data/*.json"))
+class tests:
+    def __init__(self) -> None:
 
-    for file in sat_files:
-        with open(file) as json_file:
-            data = json.load(json_file)
+        self.json_entries = [
+            "Ecology",
+            "Regions covered",
+            "Oceanography",
+            "The Arctic",
+            "Signal/sensor/ instrument type",
+            "Processing software",
+            "The Himalayas",
+            "Period covered (end)",
+            "Period covered (start)",
+            "Other regions",
+            "Climate Science",
+            "Other fields",
+            "Sensor name (if level 1)",
+            "Scientific application",
+            "Geology",
+            "Validation",
+            "RS dataset name",
+            "Antarctica",
+            "Is open data?",
+            "Dataset level",
+            "Sea Ice",
+            "Data access platform",
+            "Satellite name (if level 1)",
+            "Parameters sought",
+            "Glaciology",
+            "Hydrology",
+        ]
+
+        return None
+
+    def test_json_opening() -> None:
+        
+        sat_files = sorted(glob.glob("./data/*.json"))
+
+        for file in sat_files:
+            with open(file) as json_file:
+                data = json.load(json_file)
+                assert isinstance(data, dict)
+
+        return None
+
+    def test_json_entries():
+
+        return None
