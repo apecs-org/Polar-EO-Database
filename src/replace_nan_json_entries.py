9a7d29c71d57d487c08c0eaf07fe8938f7ceab6b
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2022-03-22 15:50:46 +0100

    Remove nan entries
    For the moment just remove nan values, we'll see later what strategy we want to follow once we have a better idea about the new database version

---
+++ src/replace_nan_json_entries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Adrien Wehrlé, University of Zurich, Switzerland
+
+"""
+
+import json
+import glob
+
+# list all database entries
+json_files = glob.glob("./data/*.json")
+
+for file in json_files:
+
+    # open JSON file and store in dict
+    with open(file, "r+") as f:
+        data = json.load(f)
+
+        # only keep non-nan values
+        data_nonan = {k: v for k, v in data.items() if v == v}
+
+        # push back modified dict
+        json.dump(data_nonan, f, indent=4)
